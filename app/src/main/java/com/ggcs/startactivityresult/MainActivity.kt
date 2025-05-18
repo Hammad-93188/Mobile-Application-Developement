60ac64fbb0671331614f68a981b511e4f1523701
Hammad Butt-93188 <93188@gcslahore.edu.pk>
2025-05-18 20:07:11 +0500

    Add files via upload

---
+++ app/src/main/java/com/ggcs/startactivityresult/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.startactivityresult
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+        val buttonOpen = findViewById<Button>(R.id.buttonOpenSecondActivity)
+
+        buttonOpen.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val result = data?.getStringExtra("user_input")
+            resultTextView.text = "Result: $result"
+        }
+    }
+
+    companion object {
+        const val REQUEST_CODE = 1
+    }
+}
